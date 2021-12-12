7790aa38eb7bffc93a1b138e42d5fb442d04d671
kthss01 <kthss01@naver.com>
2021-12-12 19:10:40 +0900

    Login with Session Cookies

--- tutorial/scheduled_tweets/app/controllers/registrations_controller.rb
+++ tutorial/scheduled_tweets/app/controllers/registrations_controller.rb
@@ -15,6 +15,7 @@ class RegistrationsController < ApplicationController
         # @user = User.new(params[:user])
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to root_path, notice: "Successfully created account"
         else
             # flash[:alert] = "Something was wrong" # 이렇게 안해도 forms에서 알아서 에러 뜸
